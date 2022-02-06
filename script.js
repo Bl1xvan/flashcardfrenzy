a0fbbaae746d58bea87f2f0c56ccddd634242221
Bl1xvan <cflavien1@my.brookdalecc.edu>
2022-02-06 10:07:07 -0500

    added delete buttons

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-const cards = document.querySelectorAll(".card");
-const deck = [];
+const subjects = document.querySelector("[data-subject]");
+const object = document.querySelector("[data-object]");
+const predicate = document.querySelector("[data-predicate]");
+const separate = document.querySelector("[data-separate]");
+const flashcardcont = document.querySelector(".flashcard-cont");
+const submit = document.querySelector(".submit");
+const deletesubj = document.querySelector("[data-delete-subj]");
+const deleteflash = document.querySelector("[data-delete-flash]");
 
-cards.forEach(card => {
-    console.log("Atenolol   " + card.innerText + " is a side effect of....");
+submit.addEventListener("click", function(){
+    let addCards = subjects.value.split(separate.value);
+    addCards.forEach(add =>{
+        let para = document.createElement("p");
+        para.className = "card"
+        para.textContent = object.value + separate.value + add + " " + predicate.value;
+        flashcardcont.appendChild(para);
+    })
 })
+
+
+
+
+
