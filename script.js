ac72e3e2067784b81067dc0490a9ab9a55dc73d7
Bl1xvan <cflavien1@my.brookdalecc.edu>
2022-02-07 11:03:56 -0500

    added styles left-side and right side

--- script.js
+++ script.js
@@ -6,16 +6,20 @@ const flashcardcont = document.querySelector(".flashcard-cont");
 const submit = document.querySelector(".submit");
 const deletesubj = document.querySelector("[data-delete-subj]");
 const deleteflash = document.querySelector("[data-delete-flash]");
+const copyflash = document.querySelector("[data-copy-flash]");
 
 submit.addEventListener("click", function(){
     let addCards = subjects.value.split(separate.value);
     addCards.forEach(add =>{
-        let para = document.createElement("p");
+        let para = document.createElement("input");
         para.className = "card"
-        para.textContent = object.value + separate.value + add + " " + predicate.value;
+        para.setAttribute("type", "text")
+        para.value = object.value +"    "+ add + " " + predicate.value;
         flashcardcont.appendChild(para);
     })
 })
+
+///Needs to be a text area so I can copy. But how do I break them apart?
 
 deletesubj.addEventListener("click", function(){
     subjects.value = " ";
@@ -25,8 +29,7 @@ deleteflash.addEventListener("click", function(){
     const cards = document.querySelectorAll(".card");
     cards.forEach(card =>{
         card.remove();
-    })
-    
+    }) 
 })
 
 
