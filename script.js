380ba3e77da724ab185750c270f03b5d20971140
Bl1xvan <cflavien1@my.brookdalecc.edu>
2022-02-07 18:02:30 -0500

    value is sort of working

--- script.js
+++ script.js
@@ -8,27 +8,22 @@ const deletesubj = document.querySelector("[data-delete-subj]");
 const deleteflash = document.querySelector("[data-delete-flash]");
 const copyflash = document.querySelector("[data-copy-flash]");
 const copyalert = document.querySelector(".copyalert");
-const deck = [];
+let text = "";
 
 submit.addEventListener("click", function(){
     let addCards = subjects.value.split(separate.value);
     addCards.forEach(add =>{
-        deck.push(object.value +"    "+ add + " " + predicate.value);   
+        text += object.value +"    "+ add + " " + predicate.value + '\r\n';
     })
-    flashcardcont.textContent = deck.join('\r\n');
+    flashcardcont.value = text;
 })
-
-///Needs to be a text area so I can copy. But how do I break them apart?
 
 deletesubj.addEventListener("click", function(){
     subjects.value = " ";
 })
 
 deleteflash.addEventListener("click", function(){
-    const cards = document.querySelectorAll(".card");
-    cards.forEach(card =>{
-        card.remove();
-    }) 
+    text = "";
 })
 
 copyflash.addEventListener("click", function(){
