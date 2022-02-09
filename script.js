78700204718addc0f1f48911b9eefa4a1c31d77e
Bl1xvan <cflavien1@my.brookdalecc.edu>
2022-02-09 12:24:41 -0500

    starting the loop process

--- script.js
+++ script.js
@@ -4,11 +4,10 @@ class Cardobject {
         this.addItem = additem;
         this.subjectItem = subjitem;
         this.predicateItem = preditem;
-        this.textItem = function(){
-            return this.objectItem + this.addItem + this.subjectItem +" "+ this.predicateItem;
-        }
     }
 }
+////add parameters in the constructor?
+//// before the main text array is converted to a string, it must check for matches
 
 const subjects = document.querySelector("[data-subject]");
 const object = document.querySelector("[data-object]");
@@ -20,21 +19,40 @@ const deletesubj = document.querySelector("[data-delete-subj]");
 const deleteflash = document.querySelector("[data-delete-flash]");
 const copyflash = document.querySelector("[data-copy-flash]");
 const copyalert = document.querySelector(".copyalert");
+let arrayb4Chng = [];
 let text = [];
+
 
 flashcardcont.addEventListener("keyup", function(){
     text = flashcardcont.value;
 })
 
-const cardItem1 = Cardobject.textItem("Ed Edd n Eddy", "    ", "Ed", "is a character from" );
-const cardItem2 = Cardobject.textItem("Ed Edd n Eddy", "    ", "Edd", "is a character from" );
-const cardItem3 = Cardobject.textItem("Ed Edd n Eddy", "    ", "Eddy", "is a character from");
+Cardobject.prototype.txttoChng = function(){
+    arrayb4Chng.push({item1: this.objectItem, item2: this.addItem, item3: this.subjectItem, item4: this.predicateItem});
+}
 
-text.push(cardItem1);
-text.push(cardItem2);
-text.push(cardItem3);
+/////need to compare item3's within the array
 
-console.log(text);
+const cardItem1 = new Cardobject("Ed Edd n Eddy", "    ", "Ed", "is a character from" );
+const cardItem2 = new Cardobject("Ed Edd n Eddy", "    ", "Edd", "is a character from" );
+const cardItem3 = new Cardobject("Ed Edd n Eddy", "    ", "Eddy", "is a character from");
+const cardItem4 = new Cardobject("Ed Edd n Eddy", "    ", "Eddy", "is a character from");
+
+text.push(cardItem1.txttoChng());
+text.push(cardItem2.txttoChng());
+text.push(cardItem3.txttoChng());
+text.push(cardItem4.txttoChng())
+
+
+
+console.log(arrayb4Chng);
+for(let x in arrayb4Chng){
+    let mxObj = [];
+    console.log(arrayb4Chng[x].item1 + " " + arrayb4Chng[x].item3);
+}
+
+
+
 ///s ubmit.addE ventListener("click", function(){
 
 //} )
